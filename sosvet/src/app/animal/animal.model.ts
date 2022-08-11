623d8d21b2e482b86fb90af7453c5414bbe6d6bf
LMCouto29 <109558597+LMCouto29@users.noreply.github.com>
2022-08-11 20:06:44 +0100

    sosvet

---
+++ sosvet/src/app/animal/animal.model.ts
@@ -0,0 +1,9 @@
+export interface Animal {
+    objectId?: string
+    name: string
+    spicies: string
+    breed : string
+    age:number
+    owner:string
+    contact:string
+}
---
+++ sosvet/src/app/question/question.component.ts
@@ -0,0 +1,78 @@
+import { Question } from '../question/question.model';
+import { UserAnswer } from './user-answer.model'; 
+import { QuestionService } from './../question/question.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
+import { TmplAstRecursiveVisitor } from '@angular/compiler';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent implements OnInit {
+
+respUser: Boolean; 
+options: string[] = ['true', 'false'];
+
+userAnswer:UserAnswer = {
+
+  value : false,
+  userId:"",
+  questionId:""
+
+}
+
+  question: Question = {
+    Id: "",
+    Description: "",
+    IsPreScreening: true,
+    IsLast : true,
+    Order:0,
+    Group:"",
+    NextQuestion:0,
+    Message:"",
+    Answers: true,
+    
+  }
+
+  
+
+
+  constructor(private questionService: QuestionService,
+    private router: Router) { }
+
+ngOnInit(): void {
+  this.getQuestion();
+      
+}
+
+
+getQuestion() { // mudar GET para get
+   this.userAnswer.value = this.respUser;
+   this.userAnswer.questionId = this.question.Id
+    this.questionService.getQuestion(this.question,this.userAnswer).subscribe(res=> {
+      this.question = res
+      if(this.question.IsLast == true || this.question?.Id == "" || this.userAnswer.value ==true){
+        this.questionService.showMessage('Pré-Triagem criada!' + this.question.Message) 
+        this.router.navigate(['/animal']  )
+          
+      } if(this.userAnswer.value == true)
+      this.questionService.showMessage('Pré-Triagem criada!'+ this.question.Message) 
+
+
+      this.router.navigate(['/question'])
+     
+    })
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/question'])
+  }
+}
+
+
+
+
